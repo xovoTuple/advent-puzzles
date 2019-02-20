7ea7c6e9837ddc3430a81f9bdc350334eee649db
xovoTuple <grahamkhigh@gmail.com>
2019-02-20 15:30:51 -0500

    refactored into functions

--- 1b/tool.py
+++ 1b/tool.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-inputs = []
-s = 0
-outputs = []
-with open('input.txt', 'r') as f:
-    for i in f:
-        inputs.append(i)
-		
-inputs = list(map(int,inputs))
+def main():
+	run_program()
 
-for x in inputs:
-	s = s + x
-	outputs.append(s)
-	for x in outputs:
-	   
+def run_program():
+		get_output(get_input())
+
+def get_input():
+	inputs = []
+	with open('input.txt', 'r') as f:
+		for i in f:
+			inputs.append(i) #create list of input frequencies. str at this point!
+		
+		inputs = list(map(int,inputs)) #map str to int
+		return inputs
+
+def get_output(inputs):
+	outputs = []
+	s = 0
+	for x in inputs:
+		s = s + x
+		outputs.append(s) #create list of output frequencies
+	length = len(outputs)
+	print('Working with {} output values'.format(length))
 	
-print(outputs)
-	
+if __name__ == "__main__": main()
