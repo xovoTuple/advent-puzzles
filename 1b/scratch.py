9f9fd0f69b55d87dc37aa771e1d068aeedef7b69
xovoTuple <grahamkhigh@gmail.com>
2019-04-26 16:32:59 -0400

    testing comparator

--- 1b/scratch.py
+++ 1b/scratch.py
@@ -1,38 +1,11 @@
-def get_input():
-	inputs = []
-	with open('input.txt', 'r') as f:
-		for i in f:
-			inputs.append(i) #create list of input frequencies. str at this point!
-		
-		inputs = list(map(int,inputs)) #map str to int
-		return inputs		
-
-def get_output(inputs):
-	outputs = []
-	s = 0
+frequency = 0
+inputs = []
+with open('input.txt', 'r') as f:
+	for i in f:
+		inputs.append(i) 
+	inputs = list(map(int,inputs))
 	for x in inputs:
-		s = s + x
-		outputs.append(s)
-	length = len(outputs)
-	return outputs, length
-
-def comparator(outputs, length):
-	matched_frequencies = []
-	print(length)
-	outputs = outputs[0]
-	#for i in length:
-	#	for j in range(i + 1, length):
-	#		if outputs[i] == outputs[j]:
-	#			matched_frequencies.append()
-	#			return matched_frequencies
-
-	#no_of_matches = len(matched_frequencies)
-	#print('found {} output values'.format(length))
-
-inputs = get_input()
-outputs, length  = get_output(inputs)
-comparator(outputs, length)
-
-
-
-
+		frequency += x
+	print(frequency)
+	print(len(inputs))
+	
--- 1b/tool.py
+++ 1b/tool.py
@@ -1,35 +1,44 @@
 def get_input():
 	inputs = []
-	with open('input.txt', 'r') as f:
+	inputs_test = [1, -2, 3, 1, 1, -2, 3, 1]
+	with open('input.txt', 'r') as f: #read input text file
 		for i in f:
 			inputs.append(i) #create list of input frequencies. str at this point!
 		
 		inputs = list(map(int,inputs)) #map str to int
-		return inputs		
+		return inputs, inputs_test
 
-def get_output(inputs):
+def get_output(inputs, inputs_test):
 	outputs = []
+	outputs_test = [0]
 	s = 0
+	s_test = 0
 	for x in inputs:
 		s = s + x
 		outputs.append(s)
 	length = len(outputs)
 	if length != len(inputs) : print("input and output lists are unequal length") #make sure input and output lists have same number of elements
-	return outputs, length
+	for x in inputs_test:
+		s_test = s_test + x
+		outputs_test.append(s_test)
+	length = len(outputs)
+	length_test = len(outputs_test)
+	return outputs, outputs_test, length, length_test
 
-def comparator(outputs, length):
+def comparator(outputs, outputs_test, length, length_test):
 	matched_frequencies = []
-	print(length)
-	outputs = outputs[0]
-	#for i in length:
-	#	for j in range(i + 1, length):
-	#		if outputs[i] == outputs[j]:
-	#			matched_frequencies.append()
-	#			return matched_frequencies
+	matched_frequencies_test = [0]
+	print("length of outputs list is: {}".format(length_test))
+	#outputs = outputs[0]
+	print("outputs list is {}".format(outputs_test))
+	for x in outputs_test:
+		for y in range(x + 1, length_test - 1):
+			print("outputs_test[x] is {}, outputs_test[y] is {}".format(outputs_test[x], outputs_test[y]))
+			if outputs_test[x] == outputs_test[y]:
+				print("match found at index x =", x)
+				print("match value ", matched_frequencies_test.append(outputs_test[x]))
+	#print(len(matched_frequencies))
 
-	#no_of_matches = len(matched_frequencies)
-	#print('found {} output values'.format(length))
-
-inputs = get_input()
-outputs, length  = get_output(inputs)
-comparator(outputs, length)
+inputs, inputs_test	= get_input()
+outputs, outputs_test, length, length_test  = get_output(inputs, inputs_test)
+comparator(outputs, outputs_test, length, length_test)
